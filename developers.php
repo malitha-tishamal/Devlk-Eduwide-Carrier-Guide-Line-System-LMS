50e7ba269580c4b754721d039bd340501ccd8d92
malitha-tishamal <malithatishamal@gmail.com>
2025-02-16 17:40:07 +0530

    add developer page

--- developers.php
+++ developers.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Developer Team - MediQ</title>
+    <title>Developer Team - EduWide</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -19,8 +19,8 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
-                <img src="assets\images\logos\favicon.png " width="35px">
-                <span class="d-none d-lg-block">Medi-Q</span>
+                <img src="assets\images\logos\favicon.jpg " width="35px">
+                <span class="d-none d-lg-block">EduWide</span>
             </a>
         </div><!-- End Logo -->
     </header><!-- End Header -->
@@ -32,27 +32,11 @@
             <!-- Team Member 1-->
             <div class="col-md-3">
                 <div class="team-card text-center">
-                    <img src="https://via.placeholder.com/100" alt="Team Member">
-                    <h4 class="fw-bold">Supun Charuka</h4>
+                    <img src="assets\images\Developers\malitha3.jpg" alt="Team Member">
+                    <h5 class="fw-bold">Malitha Tishmal</h5>
                     <p>Full Stack Developer</p>
                     <div class="team-icons">
-                        <a href="#" target="_blank"><i class="bi bi-globe"></i></a>
-                        <a href="#" target="_blank"><i class="bi bi-github"></i></a>
-                        <a href="#" target="_blank"><i class="bi bi-linkedin"></i></a>
-                        <a href="#" target="_blank"><i class="bi bi-facebook"></i></a>
-                        <a href="#" target="_blank"><i class="bi bi-envelope"></i></a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Team Member 2 -->
-            <div class="col-md-3">
-                <div class="team-card text-center">
-                    <img src="https://via.placeholder.com/100" alt="Team Member">
-                    <h4 class="fw-bold">Malitha Tishmal</h4>
-                    <p>Full Stack Developer</p>
-                    <div class="team-icons">
-                        <a href="https://malitha-tishamal.github.io/blog-malitha-tishamal/" target="_blank"><i class="bi bi-globe"></i></a>
+                        <a href="https://malitha-tishamal.github.io/blog" target="_blank"><i class="bi bi-globe"></i></a>
                         <a href="https://github.com/malitha-tishamal" target="_blank"><i class="bi bi-github"></i></a>
                         <a href="https://www.linkedin.com/in/malitha-tishamal" target="_blank"><i class="bi bi-linkedin"></i></a>
                         <a href="https://www.facebook.com/malitha.tishamal" target="_blank"><i class="bi bi-facebook"></i></a>
@@ -61,11 +45,27 @@
                 </div>
             </div>
 
+            <!-- Team Member 2 -->
+            <div class="col-md-3">
+                <div class="team-card text-center">
+                    <img src="assets\images\Developers\sandeepa.jpg" alt="Team Member">
+                    <h5 class="fw-bold">Malith Sandeepa</h5>
+                    <p>Frontend Developer</p>
+                    <div class="team-icons">
+                        <a href="#" target="_blank"><i class="bi bi-globe"></i></a>
+                        <a href="https://github.com/KVMSANDEEPA" target="_blank"><i class="bi bi-github"></i></a>
+                        <a href="www.linkedin.com/in/malith-sandeepa" target="_blank"><i class="bi bi-linkedin"></i></a>
+                        <a href="https://www.facebook.com/profile.php?id=100071177107363" target="_blank"><i class="bi bi-facebook"></i></a>
+                        <a href="mailto:malithsandeepa1081@gmail.com" ><i class="bi bi-envelope"></i></a>
+                    </div>
+                </div>
+            </div>
+
             <!-- Team Member 3 -->
             <div class="col-md-3">
                 <div class="team-card text-center">
-                    <img src="https://via.placeholder.com/100" alt="Team Member">
-                    <h4 class="fw-bold">Malith Sandeepa</h4>
+                    <img src="assets\images\Developers\nihari.jpg" alt="Team Member">
+                    <h5 class="fw-bold">Matheesha Nihari</h5>
                     <p>Frontend Developer</p>
                     <div class="team-icons">
                         <a href="#" target="_blank"><i class="bi bi-globe"></i></a>
@@ -80,8 +80,23 @@
             <!-- Team Member 4 -->
             <div class="col-md-3">
                 <div class="team-card text-center">
-                    <img src="https://via.placeholder.com/100" alt="Team Member">
-                    <h4 class="fw-bold">Nishara de Silva</h4>
+                    <img src="assets\images\Developers\dewmini.jpg" alt="Team Member">
+                    <h5 class="fw-bold">Hima Devindi</h5>
+                    <p>Frontend Developer</p>
+                    <div class="team-icons">
+                        <a href="#" target="_blank"><i class="bi bi-globe"></i></a>
+                        <a href="https://github.com/Tharu003" target="_blank"><i class="bi bi-github"></i></a>
+                        <a href="https://www.linkedin.com/in/nishara-de-silva-992409329/" target="_blank"><i class="bi bi-linkedin"></i></a>
+                        <a href="#" target="_blank"><i class="bi bi-facebook"></i></a>
+                        <a href="mailto:tharushinishara2003@gmail.com" ><i class="bi bi-envelope"></i></a>
+                    </div>
+                </div>
+            </div>
+
+        <div class="col-md-3">
+                <div class="team-card text-center">
+                    <img src="assets\images\Developers\amandi.jpg" alt="Team Member">
+                    <h5 class="fw-bold">Amandi Kaushalya</h5>
                     <p>Frontend Developer</p>
                     <div class="team-icons">
                         <a href="#" target="_blank"><i class="bi bi-globe"></i></a>
--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
                             <div class="d-flex justify-content-center py-4">
                                 <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                    <img src="assets/images/logos/mediq-logo.png" alt="" style="max-height:130px;">
+                                    <img src="assets/images/logos/eduwide-logo.png" alt="" style="max-height:130px;">
                                     <!-- <span class="d-none d-lg-block">MediQ</span> -->
                                 </a>
                             </div><!-- End Logo -->
