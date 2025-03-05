54ed88dc5103b86bd33c1bb51c530915dcb7c37e
malitha-tishamal <malithatishamal@gmail.com>
2025-03-05 19:28:55 +0530

    edit

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; Copyright <strong><span>MediQ</span></strong>. All Rights Reserved.
+        &copy; Copyright <strong><span>EduWide</span></strong>. All Rights Reserved.
     <div class="credits">
-        Developed by <a href="">Devlk</a>
+        Developed by <a href="">Devlk Team</a>
     </div>
 </footer><!-- End Footer -->
